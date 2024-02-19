a538a85fd1a9c8f7a93bbb2e41c6e2298f39db67
tenado <690784374@qq.com>
2024-02-19 12:06:48 +0800

    1

---
+++ src/components/content.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content-wrap">
+    12121
+    <el-button type="primary">Primary</el-button>
+    <!-- <el-form label-position="right" label-width="80px" :model="formRule">
+      <el-form-item label="名称">
+        <el-input v-model="formRule.surname"></el-input>
+      </el-form-item>
+
+    </el-form> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      formRule: {
+        surname: "",
+      },
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>121</div>
+  <div class="wrap">
+    <Content></Content>
+  </div>
 </template>
 
 <script>
+import Content from "./components/content.vue";
 export default {
-
+  components: {
+    Content,
+  },
 }
 
 </script>
