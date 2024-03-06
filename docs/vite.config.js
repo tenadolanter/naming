42b7fecee43ea5af8ccadd80c58f08ceec82fdbc
tenado <690784374@qq.com>
2024-03-06 19:14:33 +0800

    1

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
   build: {
     cssTarget: "chrome83",
   },
-})
+  plugins: [
+    styleImport({
+      libs: [
+        {
+          libraryName: "element-plus",
+          resolveStyle: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`;
+          },
+        },
+      ],
+    }),
+  ],
+});
