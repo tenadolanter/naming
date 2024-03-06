1f8a5f3106fd24c8fadf6f07884e89530674d2bc
tenadolanter <tenadolanter@users.noreply.github.com>
2024-03-06 11:56:08 +0000

    deploy: 015761a4b39156ca88c696ab08b80ec06adf893e

---
+++ assets/app.D-2jJSnz.js
@@ -0,0 +1,116 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import { j as inBrowser, ax as useUpdateHead, ay as RouterSymbol, az as initData, aA as dataSymbol, aB as Content, aC as ClientOnly, aD as siteDataRef, aE as createSSRApp, aF as createRouter, aG as pathToFile, aH as __vitePreload, d as defineComponent, u as useData, l as onMounted, z as watchEffect, aI as usePrefetch, aJ as useCopyCode, aK as useCodeGroups, aL as h } from "./chunks/framework.C1ex38rk.js";
+import { t as theme } from "./chunks/theme.hkcU7DxR.js";
+function resolveThemeExtends(theme2) {
+  if (theme2.extends) {
+    const base = resolveThemeExtends(theme2.extends);
+    return {
+      ...base,
+      ...theme2,
+      async enhanceApp(ctx) {
+        if (base.enhanceApp)
+          await base.enhanceApp(ctx);
+        if (theme2.enhanceApp)
+          await theme2.enhanceApp(ctx);
+      }
+    };
+  }
+  return theme2;
+}
+const Theme = resolveThemeExtends(theme);
+const VitePressApp = defineComponent({
+  name: "VitePressApp",
+  setup() {
+    const { site, lang, dir } = useData();
+    onMounted(() => {
+      watchEffect(() => {
+        document.documentElement.lang = lang.value;
+        document.documentElement.dir = dir.value;
+      });
+    });
+    if (site.value.router.prefetchLinks) {
+      usePrefetch();
+    }
+    useCopyCode();
+    useCodeGroups();
+    if (Theme.setup)
+      Theme.setup();
+    return () => h(Theme.Layout);
+  }
+});
+async function createApp() {
+  const router = newRouter();
+  const app = newApp();
+  app.provide(RouterSymbol, router);
+  const data = initData(router.route);
+  app.provide(dataSymbol, data);
+  app.component("Content", Content);
+  app.component("ClientOnly", ClientOnly);
+  Object.defineProperties(app.config.globalProperties, {
+    $frontmatter: {
+      get() {
+        return data.frontmatter.value;
+      }
+    },
+    $params: {
+      get() {
+        return data.page.value.params;
+      }
+    }
+  });
+  if (Theme.enhanceApp) {
+    await Theme.enhanceApp({
+      app,
+      router,
+      siteData: siteDataRef
+    });
+  }
+  return { app, router, data };
+}
+function newApp() {
+  return createSSRApp(VitePressApp);
+}
+function newRouter() {
+  let isInitialPageLoad = inBrowser;
+  let initialPath;
+  return createRouter((path) => {
+    let pageFilePath = pathToFile(path);
+    let pageModule = null;
+    if (pageFilePath) {
+      if (isInitialPageLoad) {
+        initialPath = pageFilePath;
+      }
+      if (isInitialPageLoad || initialPath === pageFilePath) {
+        pageFilePath = pageFilePath.replace(/\.js$/, ".lean.js");
+      }
+      if (false)
+        ;
+      else {
+        pageModule = __vitePreload(() => import(
+          /*@vite-ignore*/
+          pageFilePath
+        ), true ? __vite__mapDeps([]) : void 0);
+      }
+    }
+    if (inBrowser) {
+      isInitialPageLoad = false;
+    }
+    return pageModule;
+  }, Theme.NotFound);
+}
+if (inBrowser) {
+  createApp().then(({ app, router, data }) => {
+    router.go().then(() => {
+      useUpdateHead(router.route, data.site);
+      app.mount("#app");
+    });
+  });
+}
+export {
+  createApp
+};
