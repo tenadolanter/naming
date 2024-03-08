7aacd460fb9635a5dc3e9c0a937e291cce2abda3
tenado <690784374@qq.com>
2024-03-08 11:29:11 +0800

    1

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,74 +1,147 @@
 <template>
   <div class="content-wrap">
-    <el-form :model="formValue" :rules="formRules" ref="formRef" status-icon label-width="120px" size="large">
+    <el-form
+      :model="formValue"
+      :rules="formRules"
+      ref="formRef"
+      status-icon
+      label-width="85px"
+      size="large"
+    >
       <el-form-item label="姓氏" prop="surname">
-        <el-input v-model="formValue.surname" placeholder="输入孩子的姓氏" style="width: 500px"></el-input>
+        <el-input
+          v-model="formValue.surname"
+          placeholder="输入孩子的姓氏"
+          style="width: 500px"
+        ></el-input>
       </el-form-item>
       <el-form-item label="性别" prop="sex">
-        <el-select v-model="formValue.sex" placeholder="选择孩子的性别" clearable style="width: 500px">
+        <el-select
+          v-model="formValue.sex"
+          placeholder="选择孩子的性别"
+          clearable
+          style="width: 500px"
+        >
           <el-option label="男" value="男"></el-option>
           <el-option label="女" value="女"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="阳历日期" prop="date">
-        <el-date-picker v-model="formValue.date" type="date" value-format="YYYY-MM-DD" placeholder="选择一个日期" style="width: 500px"></el-date-picker>
+        <el-date-picker
+          v-model="formValue.date"
+          type="date"
+          value-format="YYYY-MM-DD"
+          placeholder="选择一个日期"
+          style="width: 500px"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item label="时间" prop="time">
-        <el-time-picker v-model="formValue.time" value-format="HH:mm" placeholder="选择一个时间" style="width: 500px"></el-time-picker>
+        <el-time-picker
+          v-model="formValue.time"
+          value-format="HH:mm"
+          placeholder="选择一个时间"
+          style="width: 500px"
+        ></el-time-picker>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="handlerSumbit(formRef)">起名</el-button>
-        <el-button type="primary" @click="handlerReset(formRef)">重置</el-button>
+        <el-button type="primary" @click="handlerSumbit">起名</el-button>
+        <el-button type="primary" @click="handlerReset">重置</el-button>
       </el-form-item>
     </el-form>
+    <div
+      class="result"
+      v-if="
+        formInfo.names &&
+        Array.isArray(formInfo.names) &&
+        !!formInfo.names.length
+      "
+    >
+      <div class="result-title">生成结果</div>
+      <div class="result-names">
+        <div
+          class="name-item"
+          v-for="(item, index) in formInfo.names"
+          :key="index"
+        >
+          {{ formInfo.surname + item.map((ele) => ele.char).join("") }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
-import { ElButton, ElForm, ElFormItem, ElInput, ElDatePicker, ElTimePicker, ElSelect, ElOption } from 'element-plus';
-import { getYearMonthDay, getLunar, getChar8, getElement5, getCnChar } from "../utils/index.js";
-import { reactive, ref } from "vue";
-
-const formRef = ref();
-
-const formValue = reactive({
-  surname: "",
-  sex: "",
-  date: "",
-  time: "",
-})
-
-const formRules = reactive({
-  surname: [{ required: true, message: '请输入姓氏', trigger: 'blur' },],
-  sex: [{ required: true, message: '请输入性别', trigger: 'blur' },],
-  date: [{ required: true, message: '请输入日期', trigger: 'blur' },],
-  time: [{ required: true, message: '请输入时间', trigger: 'blur' },],
-})
-
-const handlerSumbit = async (formEl) => {
-  if (!formEl) return
-  const validate = await formEl.validate();
-  if(validate) {
-    // console.log("formValue", formValue);
-    handlerData(formValue);
-  }
-}
-
-const handlerReset = (formEl) => {
-  if (!formEl) return
-  formEl.resetFields();
-}
-
-const handlerData = () => {
-  let info = {  ...formValue };
-  getYearMonthDay(info);
-  getLunar(info)
-  getChar8(info)
-  getElement5(info)
-  getCnChar(info)
-  console.log(info);
-}
-
+<script>
+import {
+  ElButton,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElDatePicker,
+  ElTimePicker,
+  ElSelect,
+  ElOption,
+} from "element-plus";
+import {
+  getYearMonthDay,
+  getLunar,
+  getChar8,
+  getElement5,
+  getCnChar,
+} from "../utils/index.js";
+export default {
+  components: {
+    ElButton,
+    ElForm,
+    ElFormItem,
+    ElInput,
+    ElDatePicker,
+    ElTimePicker,
+    ElSelect,
+    ElOption,
+  },
+  data() {
+    return {
+      formValue: {
+        surname: "",
+        sex: "",
+        date: "",
+        time: "",
+      },
+      formRules: {
+        surname: [{ required: true, message: "请输入姓氏", trigger: "blur" }],
+        sex: [{ required: true, message: "请输入性别", trigger: "blur" }],
+        date: [{ required: true, message: "请输入日期", trigger: "blur" }],
+        time: [{ required: true, message: "请输入时间", trigger: "blur" }],
+      },
+      formInfo: {},
+    };
+  },
+  methods: {
+    async handlerSumbit() {
+      const formEl = this.$refs.formRef;
+      if (!formEl) return;
+      const validate = await formEl.validate();
+      if (validate) {
+        this.handlerData();
+      }
+    },
+    handlerReset() {
+      const formEl = this.$refs.formRef;
+      if (!formEl) return;
+      formEl.resetFields();
+    },
+    handlerData() {
+      let info = { ...this.formValue };
+      getYearMonthDay(info);
+      getLunar(info);
+      getChar8(info);
+      getElement5(info);
+      getCnChar(info);
+      this.formInfo = info;
+      console.log(info, this.formInfo);
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
@@ -76,5 +149,23 @@ const handlerData = () => {
   max-width: 1000px;
   margin: 0 auto;
   padding-top: 24px;
+  .result {
+    padding: 16px;
+    .result-title {
+      font-weight: bold;
+      margin-bottom: 16px;
+    }
+    .result-names {
+      display: grid;
+      grid-template-columns: auto auto auto auto;
+      .name-item {
+        text-align: center;
+        padding: 20px;
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        background-color: rgba(255, 255, 255, 0.8);
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
