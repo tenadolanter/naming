a5edc0bc299ac6b1ab48d5d8f5220572150de949
tenado <690784374@qq.com>
2025-01-31 23:26:14 +0800

    chore: 调整起名的样式

--- src/components/content.vue
+++ src/components/content.vue
@@ -4,12 +4,13 @@
       :model="formValue"
       :rules="formRules"
       ref="formRef"
-      status-icon
       label-width="85px"
       size="large"
+      label-position="top"
     >
       <el-form-item label="姓氏" prop="surname">
         <el-input
+          :disabled="generated"
           v-model="formValue.surname"
           placeholder="输入孩子的姓氏"
           clearable
@@ -17,6 +18,7 @@
       </el-form-item>
       <el-form-item label="性别" prop="sex">
         <el-select
+          :disabled="generated"
           v-model="formValue.sex"
           placeholder="选择孩子的性别"
           clearable
@@ -25,8 +27,9 @@
           <el-option label="女" value="女"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="阳历日期" prop="date">
+      <el-form-item label="出生日期(阳历)" prop="date">
         <el-date-picker
+          :disabled="generated"
           v-model="formValue.date"
           type="date"
           value-format="YYYY-MM-DD"
@@ -34,8 +37,9 @@
           style="width: 100%"
         ></el-date-picker>
       </el-form-item>
-      <el-form-item label="时间" prop="time">
+      <el-form-item label="出生时间" prop="time">
         <el-time-picker
+          :disabled="generated"
           v-model="formValue.time"
           value-format="HH:mm"
           placeholder="选择一个时间"
@@ -43,29 +47,34 @@
         ></el-time-picker>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="handlerSumbit">起名</el-button>
-        <el-button type="primary" @click="handlerReset">重置</el-button>
+        <el-button type="primary" @click="handlerSumbit" :disabled="generated">
+          起名
+        </el-button>
+        <el-button type="primary" @click="handlerReset" :disabled="false"
+          >重置</el-button
+        >
       </el-form-item>
     </el-form>
-    <div
-      class="result"
-      v-if="
-        formInfo.names &&
-        Array.isArray(formInfo.names) &&
-        !!formInfo.names.length
-      "
-    >
+    <div class="result" v-if="generated">
       <div class="result-title">生成结果</div>
-      <div class="result-names">
-        <div
-          class="name-item"
-          v-for="(item, index) in formInfo.names"
-          :key="index"
-          @click="handlerItemClick(index)"
+      <el-table
+        :data="tableData"
+        border
+        :show-header="false"
+        empty-text="暂无数据"
+        style="width: 100%"
+        @cell-click="handlerItemClick"
+      >
+        <el-table-column
+          v-for="item in columns"
+          :key="item.prop"
+          :prop="item.prop"
         >
-          {{ formInfo.surname + item.map((ele) => ele.char).join("") }}
-        </div>
-      </div>
+          <template #default="scope">
+            {{ getTableCellValue(scope.row[item.prop]) }}
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
@@ -80,6 +89,8 @@ import {
   ElTimePicker,
   ElSelect,
   ElOption,
+  ElTable,
+  ElTableColumn,
 } from "element-plus";
 import {
   getYearMonthDay,
@@ -98,6 +109,8 @@ export default {
     ElTimePicker,
     ElSelect,
     ElOption,
+    ElTable,
+    ElTableColumn,
   },
   data() {
     return {
@@ -108,15 +121,43 @@ export default {
         time: "",
       },
       formRules: {
-        surname: [{ required: true, message: "请输入姓氏", trigger: "blur" }],
+        surname: [
+          { required: true, message: "请输入姓氏", trigger: "blur" },
+          {
+            pattern: /^[\u4e00-\u9fa5]{1,2}$/,
+            message: "只能输入 1 到 2 个汉字",
+            trigger: "blur",
+          },
+        ],
         sex: [{ required: true, message: "请输入性别", trigger: "blur" }],
         date: [{ required: true, message: "请输入日期", trigger: "blur" }],
         time: [{ required: true, message: "请输入时间", trigger: "blur" }],
       },
       formInfo: {},
+      columns: [{ prop: "1" }, { prop: "2" }, { prop: "3" }, { prop: "4" }],
+      generated: false,
     };
   },
+  computed: {
+    tableData() {
+      let results = [];
+      const size = 5;
+      const arr = this.formInfo?.names ?? [];
+      for (let i = 0; i < arr.length; i += size) {
+        const tempArr = arr.slice(i, i + size);
+        let tempObj = {};
+        for (let j = 1; j < tempArr.length; j++) {
+          tempObj[j] = tempArr[j - 1];
+        }
+        results.push(tempObj);
+      }
+      return results;
+    },
+  },
   methods: {
+    getTableCellValue(item) {
+      return this.formInfo?.surname + item.map((ele) => ele.char).join("");
+    },
     async handlerSumbit() {
       const formEl = this.$refs.formRef;
       if (!formEl) return;
@@ -129,6 +170,7 @@ export default {
       const formEl = this.$refs.formRef;
       if (!formEl) return;
       formEl.resetFields();
+      this.generated = false;
     },
     handlerData() {
       let info = { ...this.formValue };
@@ -138,9 +180,10 @@ export default {
       getElement5(info);
       getCnChar(info);
       this.formInfo = info;
+      this.generated = true;
     },
-    handlerItemClick(index){
-      console.log(this.formInfo, this.formInfo.names[index]);
+    handlerItemClick(row, column) {
+      console.log(row[column.property]);
     },
   },
 };
@@ -157,21 +200,16 @@ export default {
       font-weight: bold;
       margin-bottom: 16px;
     }
-    .result-names {
-      display: grid;
-      grid-template-columns: auto auto auto auto;
-      .name-item {
-        text-align: center;
-        padding: 20px;
-        border: 1px solid rgba(0, 0, 0, 0.8);
-        background-color: rgba(255, 255, 255, 0.8);
-        cursor: pointer;
-        &:hover {
-          background-color: #409eff;
-          color: #fff;
-          font-weight: bold;
-        }
-      }
+  }
+  ::v-deep {
+    .el-form-item .el-form-item__label {
+      font-weight: bold;
+    }
+    .el-table .cell {
+      text-align: center;
+      padding: 8px 12px;
+      cursor: pointer;
+      font-size: 15px;
     }
   }
 }
--- src/index.vue
+++ src/index.vue
@@ -7,12 +7,8 @@
 </template>
 
 <script setup>
-import 'element-plus/dist/index.css'
-import Content from "./components/content.vue"
-import Header from "./components/header.vue"
-import Footer from "./components/footer.vue"
+import "element-plus/dist/index.css";
+import Content from "./components/content.vue";
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
 </script>
-
-<style scoped lang="scss">
-
-</style>
